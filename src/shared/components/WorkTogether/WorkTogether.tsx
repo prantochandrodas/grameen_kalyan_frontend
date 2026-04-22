8b624d762f6a74477561e158798875321fea8647
Pranto Das <prantochandrodas@gmail.com>
2026-04-22 11:03:05 +0600

    changess

--- src/shared/components/WorkTogether/WorkTogether.tsx
+++ src/shared/components/WorkTogether/WorkTogether.tsx
@@ -24,7 +24,7 @@ const WorkTogether = ({ image }: IMicroHealthSectionProps) => {
   return (
     <div className={workTogether}>
       <div className={imgContainer}>
-        <Image className={img} src={IMAGE_BASE_URL + image} width={1000}
+        <Image className={img} src={'https://admin.grameenkalyan.com' + image} width={1000}
           height={1000} alt="img" />
       </div>
       <div className={contentContainer}>
