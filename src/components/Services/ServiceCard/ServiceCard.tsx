1271d40802963d6f6cb471ed7d06c66805f9d07d
Pranto Das <108420516+prantochandrodas@users.noreply.github.com>
2026-04-30 10:54:17 +0600

    Update ServiceCard.tsx

--- src/components/Services/ServiceCard/ServiceCard.tsx
+++ src/components/Services/ServiceCard/ServiceCard.tsx
@@ -56,6 +56,7 @@ const ServiceCard = ({ reverse, serviceData }: IServiceSectionProps) => {
           height={500}
           width={500}
           priority
+          unoptimized
         />
       </div>
       <div
