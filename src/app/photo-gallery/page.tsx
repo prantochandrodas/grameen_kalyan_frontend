8b641492b66ac2d1d96aef90145e3a1fd8ecc53e
Pranto Das <prantochandrodas@gmail.com>
2026-06-09 10:39:02 +0600

    new change

--- src/app/photo-gallery/page.tsx
+++ src/app/photo-gallery/page.tsx
@@ -4,9 +4,11 @@ import React from 'react';
 
 import { useFetch } from '@/shared/hook';
 import Album from '@/components/PhotoGallery/Album/Album';
+import useFetchLaravelData from '@/shared/hook/useFetchData/useFetchData';
 
 const PhotoGallery = async () => {
-  const photoAlbums = await useFetch({ url: '/photo-albums' });
+  // const photoAlbums = await useFetch({ url: '/photo-albums' });
+  const photoAlbums = await useFetchLaravelData({ url: '/photo-albums' });
 
   return (
     <div>
