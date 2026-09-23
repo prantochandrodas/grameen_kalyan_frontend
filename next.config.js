f2096c123a55563548cfd705b9b6ff1b4cf9e9d7
Pranto Das <108420516+prantochandrodas@users.noreply.github.com>
2026-09-23 13:04:51 +0600

    change

--- next.config.js
+++ next.config.js
@@ -3,32 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   optimizeFonts: true,
   eslint: {
-    // এটি বিল্ডের সময় ESLint এরর ইগনোর করবে
+    // এটি বিল্ডের সময় ESLint এরর ইগনোর করবে
     ignoreDuringBuilds: true,
   },
   distDir: 'build',
- images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'admin.grameenkalyan.com',
-      pathname: '/uploads/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'admin-grameenkalyan.stitbd.app',
-      pathname: '/**',
-    },
-    {
-      protocol: 'https',
-      hostname: 'res.cloudinary.com',
-      pathname: '/**',
-    },
-  ],
-},
-  // experimental: {
-  //   appDir: true,
-  // },
+  images: {
+    domains: ['admin.grameenkalyan.com', 'res.cloudinary.com'],
+  },
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
